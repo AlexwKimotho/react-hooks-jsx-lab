42df2fa46830f78c099b80b3b2802cdc18f15597
Alex_Wachira_Kimotho <alex.wachira@student.moringashool.com>
2022-09-22 17:36:18 +0300

    Assignment Completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id = "about">
+    <h2>About Me</h2>
+    <p>I am an Energetic and ready to learn Full Stack Developer student with core skills on how to use React JS</p>
+    <img src={image} alt= "I made this"/>
+  </div>
+  
 }
 
 export default About;
